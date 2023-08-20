1e52cb66aea24520bf4851897e3175bbf6f6b676
kqkhai06 <kqkhai06@gmail.com>
2023-08-20 12:32:56 +0700

    up code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function truncate(text, maxlength) {
-  if (text.length <= maxlength) return text;
-  const shortStr = text.slice(0, maxlength - 1);
-  return `${shortStr}\u2026`;
-}
-console.log(truncate("Easy", 4));
-console.log(truncate("Easy Frontend", 4));
+const kqkhai06 = {
+  name: "Kieu Quang Khai",
+  age: 22,
+  skills: "html",
+};
+
+console.log(kqkhai06.name);
